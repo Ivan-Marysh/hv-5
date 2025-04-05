74417e6c9f899faa860294ba36d2168d1833798a
Ivan-Marysh <166243361+Ivan-Marysh@users.noreply.github.com>
2025-04-05 16:21:26 +0300

    add 1

---
+++ java.js
@@ -0,0 +1,57 @@
+// const drink = Number(prompt("оберить drink (1 - кава, 2 - чай, 3 - сік)"))
+// switch (drink) {
+//     case 1:
+//         alert("кава");
+//         break;
+
+//         case 2:
+//             alert("чай");
+//             break;
+
+//             case 3:
+//                 alert("сік");
+//                 break;
+
+
+//     default:
+//         alert("В нас нема такого напітка");
+// }
+
+
+
+// const week = prompt("оберить день тижня ('mn', 'ts', 'wd', 'th','fr','st','sn')").trim().toLocaleLowerCase();
+// switch (week) {
+//     case 'mn':
+//         alert("робочий день");
+//         break;
+
+//         case 'ts':
+//             alert("робочий день");
+//             break;
+
+//             case 'wd':
+//                 alert("робочий день");
+//                 break;
+
+//                 case 'th':
+//                     alert("робочий день");
+//                     break;
+
+//                     case 'fr':
+//                         alert("робочий день");
+//                         break;
+
+//             case 'st':
+//                 alert("вихідний");
+//                 break;
+
+//                 case 'sn':
+//                     alert("вихідний");
+//                     break;
+
+
+//     default:
+//         alert("нет такого дня в тижне");
+// }
+
+
