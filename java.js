334ef45fb501365547bbd023f52502775daf6eb9
Ivan-Marysh <166243361+Ivan-Marysh@users.noreply.github.com>
2025-04-06 18:41:34 +0300

    add 000

--- java.js
+++ java.js
@@ -1,4 +1,4 @@
-// const drink = Number(prompt("оберить drink (1 - кава, 2 - чай, 3 - сік)"))
+// const drink = Number(prompt("оберить drink (1 - кава, 2 - чай, 3 - сік)"));
 // switch (drink) {
 //     case 1:
 //         alert("кава");
@@ -55,3 +55,92 @@
 // }
 
 
+// const nomer = Number(prompt("оберить місяць (12, 1, 2 - зима, 3, 4, 5 - весна, 6, 7, 8 - літо, 9, 10, 11 - осень)"));
+// switch (nomer) {
+//     case 1:
+//         alert("зима");
+//         break;
+
+//         case 2:
+//             alert("зима");
+//             break;
+
+//             case 3:
+//                 alert("весна");
+//                 break;
+
+//             case 4:
+//                 alert("весна");
+//                 break;
+
+//             case 5:
+//                 alert("весна");
+//                 break;
+
+//      case 6:
+//         alert("літо");
+//         break;
+
+//              case 7:
+//         alert("літо");
+//         break;
+
+//              case 8:
+//         alert("літо");
+//         break;
+
+//              case 9:
+//         alert("осень");
+//         break;
+
+//         case 10:
+//             alert("осень");
+//             break;
+
+//             case 11:
+//                 alert("осень");
+//                 break;
+
+//                 case 12:
+//                     alert("зима");
+//                     break;
+
+
+//     default:
+//         alert("нема такого номера місяца");
+// }
+
+
+// const color = prompt("оберить color ('червоний', 'зелений', 'жовтий')");
+// switch (color) {
+//     case 'червоний':
+//         alert("стоп");
+//         break;
+
+//         case 'зелений':
+//             alert("йти");
+//             break;
+
+//             case 'жовтий':
+//                 alert("чекати");
+//                 break;
+
+
+//     default:
+//         alert("нема в списку такого color");
+// }
+
+// let result
+// const number = 18;
+// const number2 = 0;
+// if (number / number2) {
+//     if (number2 === 0) {
+//         console.log();("Помилка: ділення на нуль!");
+//     }
+  
+// } else {
+//     result = number / number2;
+// }
+// if (result !== undefined) {
+//     console.log();(`Результат: ${number1} / ${number2} = ${result}`);
+// }
