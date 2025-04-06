2017013f13680b466a7c6de995e861d14df874df
Ivan-Marysh <166243361+Ivan-Marysh@users.noreply.github.com>
2025-04-06 19:06:13 +0300

    add 98

--- java.js
+++ java.js
@@ -1,146 +1,146 @@
-// const drink = Number(prompt("оберить drink (1 - кава, 2 - чай, 3 - сік)"));
-// switch (drink) {
-//     case 1:
-//         alert("кава");
-//         break;
+const drink = Number(prompt("оберить drink (1 - кава, 2 - чай, 3 - сік)")).trim().toLocaleLowerCase();
+switch (drink) {
+    case 1:
+        alert("кава");
+        break;
 
-//         case 2:
-//             alert("чай");
-//             break;
+        case 2:
+            alert("чай");
+            break;
 
-//             case 3:
-//                 alert("сік");
-//                 break;
-
-
-//     default:
-//         alert("В нас нема такого напітка");
-// }
+            case 3:
+                alert("сік");
+                break;
 
 
-
-// const week = prompt("оберить день тижня ('mn', 'ts', 'wd', 'th','fr','st','sn')").trim().toLocaleLowerCase();
-// switch (week) {
-//     case 'mn':
-//         alert("робочий день");
-//         break;
-
-//         case 'ts':
-//             alert("робочий день");
-//             break;
-
-//             case 'wd':
-//                 alert("робочий день");
-//                 break;
-
-//                 case 'th':
-//                     alert("робочий день");
-//                     break;
-
-//                     case 'fr':
-//                         alert("робочий день");
-//                         break;
-
-//             case 'st':
-//                 alert("вихідний");
-//                 break;
-
-//                 case 'sn':
-//                     alert("вихідний");
-//                     break;
+    default:
+        alert("В нас нема такого напітка");
+}
 
 
-//     default:
-//         alert("нет такого дня в тижне");
-// }
+
+const week = prompt("оберить день тижня ('mn', 'ts', 'wd', 'th','fr','st','sn')").trim().toLocaleLowerCase();
+switch (week) {
+    case 'mn':
+        alert("робочий день");
+        break;
+
+        case 'ts':
+            alert("робочий день");
+            break;
+
+            case 'wd':
+                alert("робочий день");
+                break;
+
+                case 'th':
+                    alert("робочий день");
+                    break;
+
+                    case 'fr':
+                        alert("робочий день");
+                        break;
+
+            case 'st':
+                alert("вихідний");
+                break;
+
+                case 'sn':
+                    alert("вихідний");
+                    break;
 
 
-// const nomer = Number(prompt("оберить місяць (12, 1, 2 - зима, 3, 4, 5 - весна, 6, 7, 8 - літо, 9, 10, 11 - осень)"));
-// switch (nomer) {
-//     case 1:
-//         alert("зима");
-//         break;
-
-//         case 2:
-//             alert("зима");
-//             break;
-
-//             case 3:
-//                 alert("весна");
-//                 break;
-
-//             case 4:
-//                 alert("весна");
-//                 break;
-
-//             case 5:
-//                 alert("весна");
-//                 break;
-
-//      case 6:
-//         alert("літо");
-//         break;
-
-//              case 7:
-//         alert("літо");
-//         break;
-
-//              case 8:
-//         alert("літо");
-//         break;
-
-//              case 9:
-//         alert("осень");
-//         break;
-
-//         case 10:
-//             alert("осень");
-//             break;
-
-//             case 11:
-//                 alert("осень");
-//                 break;
-
-//                 case 12:
-//                     alert("зима");
-//                     break;
+    default:
+        alert("нет такого дня в тижне");
+}
 
 
-//     default:
-//         alert("нема такого номера місяца");
-// }
+const nomer = Number(prompt("оберить місяць (12, 1, 2 - зима, 3, 4, 5 - весна, 6, 7, 8 - літо, 9, 10, 11 - осень)")).toLocaleLowerCase();;
+switch (nomer) {
+    case 1:
+        alert("зима");
+        break;
+
+        case 2:
+            alert("зима");
+            break;
+
+            case 3:
+                alert("весна");
+                break;
+
+            case 4:
+                alert("весна");
+                break;
+
+            case 5:
+                alert("весна");
+                break;
+
+     case 6:
+        alert("літо");
+        break;
+
+             case 7:
+        alert("літо");
+        break;
+
+             case 8:
+        alert("літо");
+        break;
+
+             case 9:
+        alert("осень");
+        break;
+
+        case 10:
+            alert("осень");
+            break;
+
+            case 11:
+                alert("осень");
+                break;
+
+                case 12:
+                    alert("зима");
+                    break;
 
 
-// const color = prompt("оберить color ('червоний', 'зелений', 'жовтий')");
-// switch (color) {
-//     case 'червоний':
-//         alert("стоп");
-//         break;
-
-//         case 'зелений':
-//             alert("йти");
-//             break;
-
-//             case 'жовтий':
-//                 alert("чекати");
-//                 break;
+    default:
+        alert("нема такого номера місяца");
+}
 
 
-//     default:
-//         alert("нема в списку такого color");
-// }
+const color = prompt("оберить color ('червоний', 'зелений', 'жовтий')").toLocaleLowerCase();;
+switch (color) {
+    case 'червоний':
+        alert("стоп");
+        break;
 
-// let result
-// const number = 18;
-// const number2 = 0;
-// if (number / number2) {
-//     if (number2 === 0) {
-//         console.log();("Помилка: ділення на нуль!");
-//     }
+        case 'зелений':
+            alert("йти");
+            break;
+
+            case 'жовтий':
+                alert("чекати");
+                break;
+
+
+    default:
+        alert("нема в списку такого color");
+}
+
+let result
+const number = 18;
+const number2 = 0;
+if (number / number2) {
+    if (number2 === 0) {
+        console.log();("Помилка: ділення на нуль!");
+    }
   
-// } else {
-//     result = number / number2;
-// }
-// if (result !== undefined) {
-//     console.log();(`Результат: ${number1} / ${number2} = ${result}`);
-// }
+} else {
+    result = number / number2;
+}
+if (result !== undefined) {
+    console.log();(`Результат: ${number1} / ${number2} = ${result}`);
+}
